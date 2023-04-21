f7b06c4ab089061a3ccc2f69588699259b1de87c
VelosoRaptor <re.veloso@duocuc.cl>
2023-04-20 21:12:23 -0400

    ahora con JS

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log("Estamos Conectados");
+document.write("Mensaje desde JS 1");
+document.write("<h1>Mensaje desde JS 2<h1>");
+//alert("Mensaje de ALERTAAAAA!!!!");
+var v_var1 = 4.5;
+var v_var2 = 0.9;
+var v_var3 = 1.0;
+var prom = (v_var1+v_var2+v_var3)/3;
+
+console.log("El promedio de sus notas es:" ,prom);
+
+
+if (prom<4) {
+    document.write("Ha reprobado la asignatura")
+}
+else {
+    document.write("Ha aprobado la asignatura")
+
+    }
+
+
+
